ac97cab9ede07af9bd2db836e32a5848ee857a46
Sat0o0 <ver.buf@gmail.com>
2022-03-16 21:42:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ import seaborn as sns
 import string
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.mertics import accuracy_score
+from sklearn.metrics import accuracy_score
 
 
 #read excel file as Pandas DataFrame
-df = pd.read_excel("data/titanic.xlsx")
+df = pd.read_excel("D:/EAP/PLHPRO/TitanicProject/train.xlsx")
+print(type(df))
+print(df)
 
 #open excel workbook
-workbook = openpyxl.load_workbook("data/titanic.xlsx")
+wb = openpyxl.load_workbook("D:/EAP/PLHPRO/TitanicProject/train.xlsx")
+print(type(wb))
+print(wb)
 
 #worksheet object
-worksheet = workbook[workbook.sheetnames[0]]
+ws = wb[wb.sheetnames[0]]
+print(type(ws))
+print(ws)
 
 #indices corresponding to all hidden colums
 hidden_cols_idx = [
     string.ascii_uppercase.index(col_name)
     for col_name in [
         col
-        for col, dimension in worksheet.column_dimensions.items()
-        is dimension.hidden
+        for col, dimension in ws.column_dimensions.items()
+        if dimension.hidden
     ]
 ]
 
@@ -34,7 +40,7 @@ hidden_cols_name = df.columns[hidden_cols_idx].tolist()
 
 #drop hidden columns and rows
 df.drop(hidden_cols_name, axis=1, inplace=True)
-df.drop(hidden_rows_idx, axis=0 , inplace=True)
+df.drop(hidden_cols_idx, axis=0 , inplace=True)
 
 #reset index
 df.reset_index(drop=True, inplace=True)
